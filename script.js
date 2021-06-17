3627ca5d669bc198b14308e201bf9f6708a3e17e
Mutlu Kokdemir <kokdemir.mutlu@gmail.com>
2021-06-17 20:16:08 +0300

    added progress bar functionality

--- script.js
+++ script.js
@@ -60,6 +60,20 @@ function nextSong(){
     playSong()
 }
 
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    // console.log(clickX)
+    const duration = audio.duration
+    audio.currentTime = (clickX / width) * duration
+}
+
 
 // event listeners
 playBtn.addEventListener('click', () => {
@@ -76,3 +90,9 @@ playBtn.addEventListener('click', () => {
 // change song events
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
